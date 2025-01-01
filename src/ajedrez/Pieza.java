e5836d5dc0bcb818e193e9d51e6b345307138be0
misProyectosPropios <inakifrutos@outlook.com>
2025-01-01 11:24:55 -0300

    MODIFY ajedrez/pieza.java ADD values to each pice
    ADD the chess value to all the pieces.
    Sets the king as it values 0, because it can't be captured.
    All the others pieces, have the values as in the typically chess pieces'
    value

--- src/ajedrez/Pieza.java
+++ src/ajedrez/Pieza.java
@@ -1,23 +1,30 @@
 package ajedrez;
 
 public enum Pieza {
-	PEON(1, "P"), 
-	TORRE(2, "T"), 
-    CABALLO(3, "C"), 
-    ALFIL(4, "A"), 
-    REINA(5, "Q"), 
-    REY(6, "K");
+	PEON(1, "PEON", 1), 
+	TORRE(2, "TORRE", 5), 
+    CABALLO(3, "CABALLO", 3), 
+    ALFIL(4, "ALFIL", 3), 
+    REINA(5, "REINA", 9), 
+    REY(6, "REY", 0);
+	
 	
 	private int valor;
 	private String abreviatura;
+	private int chessValue;
 
-	private Pieza(int valor, String abreviatura) {
+	private Pieza(int valor, String abreviatura, int chessValue) {
 		this.valor = valor;
 		this.abreviatura = abreviatura;
+		this.chessValue = chessValue;
 	}
 
 	public int getValor() {
 		return this.valor;
+	}
+	
+	public int getChessValue() {
+		return this.chessValue;
 	}
 
 	@Override
