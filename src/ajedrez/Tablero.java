64df4ed2f578f8f167eeb63cde0bfaf1c76900d4
misProyectosPropios <inakifrutos@outlook.com>
2025-01-01 16:52:46 -0300

    MODIFY Tablero.java. Tablero initialized with the given pieces

--- src/ajedrez/Tablero.java
+++ src/ajedrez/Tablero.java
@@ -1,27 +1,103 @@
 package ajedrez;
 
 public class Tablero {
-	int tablero[][];
+	final static int TAMANO = 8;
+	
+	private PiezaConColor tablero[][] = new PiezaConColor[TAMANO][TAMANO];
 	
 	public Tablero(int numeroCasillas) {
-		this.tablero = new int[numeroCasillas][numeroCasillas];
-		
 		//Lo inicializamos;
 		for(int i = 0; i < numeroCasillas; i++) {
 			for(int j = 0; j < numeroCasillas; j++) {
-				this.tablero[i][j] = 0;
+				this.tablero[i][j] = null;
 			}
+		}
+		
+		//Lo llenamos con las piezas correspondientes
+		//Peones
+		for (int i = 0; i < numeroCasillas; i++) {
+			this.tablero[1][i] = new PiezaConColor(Pieza.PEON, false);
+			this.tablero[6][i] = new PiezaConColor(Pieza.PEON, true);
+		}
+		
+		//Le llenamos las piezas según posiciones.csv 
+		//TODO
+		
+		//Torres negras
+		this.tablero[0][0] = new PiezaConColor(Pieza.TORRE, false);
+		this.tablero[0][7] = new PiezaConColor(Pieza.TORRE, false);
+		
+		//Caballo negro
+		this.tablero[0][1] = new PiezaConColor(Pieza.CABALLO, false);
+		this.tablero[0][6] = new PiezaConColor(Pieza.CABALLO, false);
+		
+		//Alfil negro
+		this.tablero[0][2] = new PiezaConColor(Pieza.ALFIL, false);
+		this.tablero[0][5] = new PiezaConColor(Pieza.ALFIL, false);
+		
+		//REINA NEGRA
+		this.tablero[0][3] = new PiezaConColor(Pieza.REINA, false);
+		
+		//REY NEGRO
+		this.tablero[0][4] = new PiezaConColor(Pieza.REY, false);
+		
+		//Torres blancas
+		this.tablero[7][0] = new PiezaConColor(Pieza.TORRE, true);
+		this.tablero[7][7] = new PiezaConColor(Pieza.TORRE, true);
+		
+		//Caballo negro
+		this.tablero[7][6] = new PiezaConColor(Pieza.CABALLO, true);
+		this.tablero[7][1] = new PiezaConColor(Pieza.CABALLO, true);
+				
+		//Alfil negro
+		this.tablero[7][2] = new PiezaConColor(Pieza.ALFIL, true);
+		this.tablero[7][5] = new PiezaConColor(Pieza.ALFIL, true);
+				
+		//REINA NEGRA
+		this.tablero[7][3] = new PiezaConColor(Pieza.REINA, true);
+				
+		//REY NEGRO
+		this.tablero[7][4] = new PiezaConColor(Pieza.REY, true);
+		
+	}
+	
+	
+	public void mostrarTablero() {
+		for (int i = 0; i < this.tablero.length; i++) {
+			for (int j = 0; j < this.tablero[i].length - 1; j++) {
+				System.out.print(this.tablero[i][j].getPieceEmoji() +  " | ");
+			}
+			System.out.print(this.tablero[i][this.tablero[i].length - 1].getPieceEmoji() + "\n");
 		}
 	}
 	
-	/*
-	public void mostrarTablero() {
+	public void colocarPieza(int fila, int columna, PiezaConColor pieza) {
+		this.tablero[fila][columna] = pieza;
+	}
+	
+	/**
+	 * Muestra el valor de las piezas que hay en el tablero
+	 */
+	public void calcularValorTablero() {
+		int puntosBlancas = 0;
+		int puntosNegras = 0;
+		
 		for (int i = 0; i < this.tablero.length; i++) {
 			for (int j = 0; j < this.tablero[i].length; j++) {
-				System.out.print(this.tablero[i][j] + " ");
+				if (this.tablero[i][j] == null) {
+					continue;
+				}
+				if (this.tablero[i][j].isWhite()) {
+					puntosBlancas += this.tablero[i][j].getPieza().getValor();
+				} else {
+					puntosNegras += this.tablero[i][j].getPieza().getValor();
+				}
 			}
-			System.out.println();
 		}
+		System.out.println("Puntos blancas: " + puntosBlancas);
+		System.out.println("Puntos negras: " + puntosNegras);
 	}
-	*/
+
+
+
 }
